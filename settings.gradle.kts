4298b0594594e6cf8dcddca807094e482ba6eeeb
Oscar Mwanandimai <oscar.mwana@gmail.com>
2022-11-27 16:08:18 +0100

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kata-kt-0lly"
---
+++ src/main/kotlin/com/httpoz/katakt0lly/KataKt0llyApplication.kt
@@ -0,0 +1,11 @@
+package com.httpoz.katakt0lly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KataKt0llyApplication
+
+fun main(args: Array<String>) {
+	runApplication<KataKt0llyApplication>(*args)
+}
